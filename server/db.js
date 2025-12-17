e6a31ed0f6401500667be363fe64449fe2bd020f
Imran Azizuddin <imran-aziz@mail.com>
2025-12-17 22:05:24 +0000

    fix ssl req

--- server/db.js
+++ server/db.js
@@ -2,10 +2,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Required for Render
-  }
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool; // Export the configured pool
